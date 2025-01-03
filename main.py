d57278c2f28e6886235a8238f73f589b295da37b
deucedhunter <deucedhunter@gmail.com>
2025-01-03 12:43:05 +0100

    Day 29 Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperatures = 23
+    return {"station": station, "date": date, "temperatures": temperatures}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
